cac8a59b089fa32a463f8b8f5b62bfaddfe0d0d3
1 <fattaev.el@yandex.ru>
2022-10-08 17:28:08 +0300

    changes in Classwork 4

--- Classwork 3/board/advertisement/urls.py
+++ Classwork 3/board/advertisement/urls.py
@@ -3,9 +3,9 @@ from .import views
 
 urlpatterns = [
     path('', views.advertisement_list, name='advertisement_list'),
-    path('advertisement1/', views.advertisement_details, name='advertisement_list'),
-    path('contacts/', views.contacts, name='advertisement_list'),
-    path('about/', views.About.as_view(), name='advertisement_list'),
-    path('categories/', views.categories, name='advertisement_list'),
-    path('regions/', views.Regions.as_view(), name='advertisements_list')
+    path('advertisement1/', views.advertisement_details, name='advertisement_1'),
+    path('contacts/', views.contacts, name='advertisement_contacts'),
+    path('about/', views.About.as_view(), name='advertisement_about'),
+    path('categories/', views.categories, name='advertisement_categories'),
+    path('regions/', views.Regions.as_view(), name='advertisement_regions')
 ]
--- Classwork 4/board/advertisements_app/views.py
+++ Classwork 4/board/advertisements_app/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from advertisements_app.models import Advertisement
+
+
+def advertisements_list(request, *args, **kwargs):
+    advertisements = Advertisement.objects.all()
+    return render(request, 'advertisements_app/advertisements.html', {
+        'advertisements': advertisements
+    })
